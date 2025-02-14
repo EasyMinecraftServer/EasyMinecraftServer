6691f7d4061f6e0d057b60b24b4300e1435c51aa
EngurRuzgar <ruzgar@nucceteere.xyz>
2025-02-15 01:57:59 +0300

    chore: split up functions from main

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 # Easy Minecraft Server
-Easy Minecraft Server is a CLI app I'm developing to create Minecraft servers easily and automatically
+Easy Minecraft Server is a CLI app I'm developing to create Minecraft servers easily and automatically  
 I hope to support all Minecraft version and every mod loader/plugin framework
 
 # Development
-To setup a dev env you need to install [Poetry](https://python-poetry.org/)
+## Requirements
 
-Then run these commands
+- [Rust](https://www.rust-lang.org/) (Pre-Commit Hooks)
+- [Golang](https://go.dev/) (Pre-Commit Hooks)
+- [Poetry](https://python-poetry.org/)
+
+## Setup
+
+After you install all the requirements, run these commands:
 ```
 poetry install
 poetry shell
--- easy-minecraft-server/main.py
+++ easy-minecraft-server/main.py
@@ -1,14 +1,34 @@
 import typer
+
 from rich import print
 from rich.prompt import Prompt
+from rich.markup import escape
 
 app = typer.Typer()
 
 
 @app.command()
-def main():
-    test = Prompt.ask("Test")
-    print(f"[red][bold]{test}")
+def install():
+    print("[bold red]Not Implemented[/bold red]")
+    raise typer.Abort()
+
+
+@app.command()
+def configure():
+    pick_location()
+
+
+def pick_location():
+    try:
+        location = Prompt.ask(
+            "[yellow]Where[/yellow] would you like your server to be created at?\n[grey0](Default: /opt/minecraft)[/grey0]"
+        )
+    except KeyboardInterrupt:
+        print("\n")
+        raise typer.Abort()
+    if location == "":
+        location = "/opt/minecraft"
+    print(f"Selected [bold magenta]{escape(location)}[/bold magenta]")
 
 
 if __name__ == "__main__":
