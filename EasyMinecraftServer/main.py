819dd14d117e4468320bdec386aedbb082b47354
EngurRuzgar <ruzgar@nucceteere.xyz>
2025-02-16 22:41:59 +0300

    feat: working location pick
    Signed-off-by: EngurRuzgar <ruzgar@nucceteere.xyz>

--- EasyMinecraftServer/main.py
+++ EasyMinecraftServer/main.py
@@ -1,5 +1,7 @@
 import typer
+import json
 
+from pathvalidate import sanitize_filepath
 from rich import print
 from rich.prompt import Prompt
 from rich.markup import escape
@@ -15,7 +17,13 @@ def install():
 
 @app.command()
 def configure():
-    pick_location()
+    location = pick_location()
+
+    # config = {"location": location, "version": version, "loader": loader, "ram": ram, "optimized": optimized}
+    # with open("config.json", mode="w", encoding="utf-8") as conf_file:
+    #   json.dump(config, conf_file)
+    json.dump()
+    # location: str, version: str, loader: str, ram: int, optimized: bool
 
 
 def pick_location():
@@ -28,7 +36,9 @@ def pick_location():
         raise typer.Abort()
     if location == "":
         location = "/opt/minecraft"
+    location = sanitize_filepath(file_path=location, platform="auto")
     print(f"Selected [bold magenta]{escape(location)}[/bold magenta]")
+    return location
 
 
 if __name__ == "__main__":
